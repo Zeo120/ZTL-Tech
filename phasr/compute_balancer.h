be9498b8e10f1a5955e64023d2f34537c336e76d
LVSS <your@email.com>
2026-05-24 17:54:57 +0530

    perf: Upgrade compute balancer with thread priority elevation, SMT bypass, and HugePages allocators

--- phasr/compute_balancer.h
+++ phasr/compute_balancer.h
@@ -8,7 +8,9 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <sched.h>
+#include <sys/mman.h>
 #endif
+#include <stdlib.h>
 
 // Determine number of physical cores
 static inline int get_core_count(void) {
@@ -21,16 +23,28 @@ static inline int get_core_count(void) {
 #endif
 }
 
-// Pin current thread to core index
+// Pin current thread to core index with priority elevation and physical core mapping (SMT/hyperthreading avoidance)
 static inline void pin_current_thread(int core_index) {
+    // 1. Thread priority elevation
+#if defined(_WIN32)
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+#else
+    struct sched_param param;
+    param.sched_priority = 50; // high priority
+    pthread_setschedparam(pthread_self(), SCHED_RR, &param);
+#endif
+
+    // 2. Physical core mapping (prioritize even-numbered physical cores over hyperthreaded siblings)
     int cores = get_core_count();
     if (cores <= 0) cores = 1;
+    int target_core = (core_index * 2) % cores;
+
 #if defined(_WIN32)
-    SetThreadAffinityMask(GetCurrentThread(), 1ULL << (core_index % cores));
+    SetThreadAffinityMask(GetCurrentThread(), 1ULL << target_core);
 #else
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
-    CPU_SET(core_index % cores, &cpuset);
+    CPU_SET(target_core, &cpuset);
     pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 #endif
 }
@@ -41,6 +55,39 @@ static inline void paced_sleep(int ms) {
     Sleep((DWORD)ms);
 #else
     usleep((useconds_t)ms * 1000);
+#endif
+}
+
+// Allocate high-performance memory using HugePages / Large Pages (with fallback)
+static inline void* allocate_huge_pages(size_t size) {
+#if defined(_WIN32)
+    // Attempt large pages commit (requires SeLockMemoryPrivilege enabled in OS), fallback to standard VirtualAlloc
+    void* ptr = VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE | MEM_LARGE_PAGES, PAGE_READWRITE);
+    if (!ptr) {
+        ptr = VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    }
+    return ptr;
+#else
+    // Attempt Linux HugeTLB anonymous mapping, fallback to malloc
+    void* ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
+    if (ptr == MAP_FAILED) {
+        ptr = malloc(size);
+    }
+    return ptr;
+#endif
+}
+
+// Free HugePages allocated memory
+static inline void free_huge_pages(void* ptr, size_t size) {
+    if (!ptr) return;
+#if defined(_WIN32)
+    (void)size;
+    VirtualFree(ptr, 0, MEM_RELEASE);
+#else
+    // Attempt to unmap first (checking standard size unmapping), fallback to free
+    if (munmap(ptr, size) != 0) {
+        free(ptr);
+    }
 #endif
 }
 
